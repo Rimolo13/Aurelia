bbd726e1b5898220be7abfad4316a58f70bce576
Rimolo13 <dornichmaximo@gmail.com>
2025-10-15 18:10:21 +0200

    ...

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,5 +1,5 @@
 import { REST, Routes } from "discord.js";
-import config from './config.json' with { type: 'json' };
+import config from '../configuration/config.json' with { type: 'json' };
 const token:any = config[0].token;
 const guildId:any = config[0].guildId;
 const clientId:any= config[0].clientId;
