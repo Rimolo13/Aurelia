c87e2b9afbba6dafbed619a5cf2634fe5c6455c8
Rimolo13 <dornichmaximo@gmail.com>
2025-10-18 19:36:42 +0200

    ...

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -3,16 +3,9 @@ import { exec } from "child_process";
 
 const command = {
     data: new SlashCommandBuilder().setName("update").setDescription("update the bot"),
-    execute(interaction: ChatInputCommandInteraction) {
-        exec("cd ../ && mkdir -p temp && cp -r ./configuration/* ./temp && git reset --hard HEAD && git pull && cp -r ./temp/* ./configuration && npm install && cp ./package.json ./dist && npx tsc && cd ./dist && npm install && clear",
-            async (error: Error | null) => {
-        if (error) {
-          await interaction.reply(`❌ Error: ${error.message}`);
-          return;
-        } else {
-          await interaction.reply(`✅ Update & Build successfully!`);
-        }
-      });
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.reply("🔄 Updating...");
+        exec("cd ../ && mkdir -p temp && cp -r ./configuration/* ./temp && git reset --hard HEAD && git pull && cp -r ./temp/* ./configuration && npm install && cp ./package.json ./dist && npx tsc && cd ./dist && npm install && clear");
     },
 };
 
